3dadbeb96a85eec703738e5ec5a991bf7df45bae
PessimistGod <rameshrahul0004@gmail.com>
2023-10-08 17:43:30 +0530

    Update Footer.js

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { IoBookOutline } from 'react-icons/io5'
 
 const Footer = () => {
   return (
     <footer className="text-gray-600 body-font block">
   <div className="container px-5 py-2 mx-auto flex items-center sm:flex-row flex-col">
-  <Link to={'/'} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-     <img className='w-24 aspect-auto mix-blend-multiply' src='./Logo.png' alt='Book Store' />
+  <Link to={'/'} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 justify-center">
+  <IoBookOutline size={28}/> <span className='mx-2 font-semibold '>Book Store</span>
     </Link>
     <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Books Management System
     </p>
