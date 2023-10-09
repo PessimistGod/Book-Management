fe3b18108b4e50ff9422981e3b9cd4f7d74fac82
PessimistGod <rameshrahul0004@gmail.com>
2023-10-09 16:58:59 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import BooksPage from './Pages/BooksPage';
-import AppLayout from './AppLayout';
-import Login from './Authentication/Login';
-import Signup from './Authentication/Signup';
-import Cart from './Pages/Cart';
-import CreateBook from './Pages/CreateBook';
-import EditBook from './Pages/EditBook';
-import InvalidPage from './InvalidPage';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, lazy, Suspense } from 'react'; // Import lazy and Suspense
 import Loading from './Components/Loading';
 import LoginLayout from './LoginLayout';
+
+// Lazy load these components
+const AppLayout = lazy(() => import('./AppLayout'));
+const BooksPage = lazy(() => import('./Pages/BooksPage'));
+const Login = lazy(() => import('./Authentication/Login'));
+const Signup = lazy(() => import('./Authentication/Signup'));
+const Cart = lazy(() => import('./Pages/Cart'));
+const CreateBook = lazy(() => import('./Pages/CreateBook'));
+const EditBook = lazy(() => import('./Pages/EditBook'));
+const InvalidPage = lazy(() => import('./InvalidPage'));
 
 function App() {
   return (
     <Router>
-      <AppContent />
+      {/* Wrap the content in Suspense */}
+      <Suspense fallback={<Loading />}>
+        <AppContent />
+      </Suspense>
     </Router>
   );
 }
@@ -27,7 +32,7 @@ function AppContent() {
   useEffect(() => {
     setLoading(true);
     setTimeout(() => setLoading(false), 1200);
-  }, [location.pathname]); 
+  }, [location.pathname]);
 
   return (
     <>
@@ -42,10 +47,10 @@ function AppContent() {
             <Route path="/editBook/:id" element={<EditBook />} />
           </Route>
           <Route path="/login" element={<LoginLayout />}>
-          <Route index element={<Login />} />
+            <Route index element={<Login />} />
           </Route>
           <Route path="/signup" element={<LoginLayout />}>
-          <Route index element={<Signup/>} />
+            <Route index element={<Signup />} />
           </Route>
           <Route path="*" element={<InvalidPage />} />
         </Routes>
